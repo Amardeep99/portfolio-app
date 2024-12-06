4113bd50b6cea7527b98daf5014eb3a1a68d1cde
Amardeep Singh <amardeep@hotmail.no>
2024-12-06 21:55:34 +0100

    tailwind :)

---
+++ src/assets/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Header: React.FC = () => (
+    <div className="w-screen h-32 bg-[#2C3E50] rounded-b-md flex items-center justify-center">
+        <p className="text-white text-center"> Amardeep Singh</p>
+    </div>
+)
