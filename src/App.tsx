f76501f6912c4cba496038e2addeeb199c17ab38
Amardeep Singh <amardeep@hotmail.no>
2024-12-08 20:58:45 +0100

    Darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from "react";
 import { Header } from "./assets/Header/Header"
 
 
 function App() {
+  const [darkmode, setDarkmode] = useState(false);
+  
+  useEffect(() => {
+    if (darkmode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkmode]);
 
   return (
     <>
-    <Header />
-    <div className="flex">
-      <img src="portrett.tiff" className="rounded-full w-[15%] h-[30%] mt-4 ml-4" />
-      <div className="table-column mt-8 ml-8 border-2 border-[#95A5A6] rounded-md p-4">
-        <p>25 år</p>
+    <Header darkmode={darkmode} setDarkmode={setDarkmode}/>
+    <div className="w-screen h-screen bg-white dark:bg-[#212121]">
+      <div className="flex">
+        <img src="portrett.tiff" className="rounded-full w-[15%] h-[30%] mt-4 ml-4 border-4 border-[#2C3E50] dark:border-white" />
+        <div className="table-column mt-8 ml-8 border-2 dark:bg-[#2f2f2f] bg-white 
+        border-[#2C3E50] dark:border-white rounded-md p-4 space-y-4">
+          <p className="text-black dark:text-white">25 år</p>
+          <p className="text-black dark:text-white">Full-stack C#/.NET og React+TS utivkler</p>
+        </div>
       </div>
     </div>
     </> 
--- src/assets/Header/Header.tsx
+++ src/assets/Header/Header.tsx
@@ -1,8 +1,21 @@
 import React from "react"
 
-export const Header: React.FC = () => (
-<div className="relative w-screen h-32 bg-[#2C3E50] flex items-center justify-evenly">
-    <a href="" className="text-white">Se denne koden på GitHub!</a>
-  <p className="relative text-white text-center">Amardeep Singh</p>
-</div>
-)
+interface HeaderProps {
+    darkmode: boolean;
+    setDarkmode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function Header({ darkmode, setDarkmode }: HeaderProps) {
+    return(
+        <div className="relative w-screen h-32 bg-[#2C3E50] flex items-center justify-evenly">
+            <div className="flex space-x-3 align-bottom">
+                <a href="https://github.com/Amardeep99/portfolio-app" className="text-white underline">
+                Se denne koden på GitHub!
+                </a>
+                <img src="github.png" className="h-8 w-8"/>
+            </div>
+        <p className="relative text-white text-center">Amardeep Singh</p>
+        <img src={darkmode ? "dark.png" : "light.png"} className="h-8 w-8 cursor-pointer" onClick={() => setDarkmode(!darkmode)}/>
+        </div>
+    )
+}
