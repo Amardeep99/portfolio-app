abc387fa3e62d6fa862b783e6c588b831e000025
Amardeep Singh <amardeep@hotmail.no>
2024-12-08 21:38:25 +0100

    Add CV download

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,11 @@ function App() {
       <div className="flex">
         <img src="portrett.tiff" className="rounded-full w-[15%] h-[30%] mt-4 ml-4 border-4 border-[#2C3E50] dark:border-white" />
         <div className="table-column mt-8 ml-8 border-2 dark:bg-[#2f2f2f] bg-white 
-        border-[#2C3E50] dark:border-white rounded-md p-4 space-y-4">
+        border-[#2C3E50] dark:border-white rounded-md p-4 space-y-8">
           <p className="text-black dark:text-white">25 år</p>
           <p className="text-black dark:text-white">Full-stack C#/.NET og React+TS utivkler</p>
+          <p className="text-black dark:text-white">Konsulent for Academic Works</p>
+          <p className="text-black dark:text-white">Utleid til UDI (Utlendingsdirektoratet)</p>
         </div>
       </div>
     </div>
