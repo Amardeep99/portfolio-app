de8fd5a4f8dcaa317ec237c4cebf0f58e4e9e90d
Amardeep Singh <amardeep@hotmail.no>
2024-12-08 22:08:05 +0100

    Add logos

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
     <div className="w-screen h-screen bg-white dark:bg-[#212121]">
       <div className="flex">
         <img src="portrett.tiff" className="rounded-full w-[15%] h-[30%] mt-4 ml-4 border-4 border-[#2C3E50] dark:border-white" />
+
         <div className="table-column mt-8 ml-8 border-2 dark:bg-[#2f2f2f] bg-white 
         border-[#2C3E50] dark:border-white rounded-md p-4 space-y-8">
           <p className="text-black dark:text-white">25 år</p>
@@ -26,6 +27,15 @@ function App() {
           <p className="text-black dark:text-white">Konsulent for Academic Works</p>
           <p className="text-black dark:text-white">Utleid til UDI (Utlendingsdirektoratet)</p>
         </div>
+
+        <div>
+        <div className="flex w-screen space-x-1 px-14 py-4"> 
+          <img src="csharp.png" className="w-28"/> 
+          <img src="dotnet.png" className="w-28 rounded-full"/>
+      <img src="react.png" className="w-28"/>
+
+        </div>
+      </div>  
       </div>
     </div>
     </> 
--- src/assets/Header/Header.tsx
+++ src/assets/Header/Header.tsx
@@ -6,29 +6,21 @@ interface HeaderProps {
 }
 
 export function Header({ darkmode, setDarkmode }: HeaderProps) {
-    const handleDownload = () => {
-        const link = document.createElement('a');
-        link.href = '/CV.pdf'; // Path to the file in the public folder
-        link.download = 'CV.pdf'; // The name of the file to be downloaded
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
+
 
     return(
         <div className="relative w-screen h-32 bg-[#2C3E50] flex items-center justify-between px-6">
-            <div className="flex space-x-6 align-bottom">
+            <div className="flex space-x-4 align-bottom">
                 <a href="https://github.com/Amardeep99/portfolio-app" className="text-white underline mt-2">
                 Se denne koden på GitHub!
                 </a>
                 <img src="github-dark.png" className="h-8 w-8"/>
             </div>
-        <div className="flex space-x-4 border-2 border-white px-4 py-2 rounded-2xl h-auto cursor-pointer"
-        onClick={() => handleDownload}>
-            <p className="relative text-white text-center mt-2">Last ned min CV :)</p>
-            <img src="download.png" className="h-10 w-10"/>
-        </div>
-        <img src={darkmode ? "dark.png" : "light.png"} className="h-10 w-10 cursor-pointer" onClick={() => setDarkmode(!darkmode)}/>
+            <a href="/CV.pdf" className="flex space-x-4 border-2 border-white px-4 py-2 rounded-2xl h-auto hover:border-[#3498DB]">
+                <p className="relative text-white text-center mt-2">Last ned min CV :)</p>
+                <img src="download.png" className="h-10 w-10"/>
+            </a>
+            <img src={darkmode ? "dark.png" : "light.png"} className="h-10 w-10 cursor-pointer" onClick={() => setDarkmode(!darkmode)}/>
         </div>
     )
 }
